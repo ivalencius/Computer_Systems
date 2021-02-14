27411e9764e1e2e5ce3af4be8fdb9c1cdb878067
ivalencius <valencig@bc.edu>
2021-02-14 15:52:00 -0500

    updated to compile without c99

--- convert.c
+++ convert.c
@@ -1,7 +1,6 @@
 /*
 * Author: Ilan Valencius - valencig@bc.edu
 */
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,13 +8,15 @@
 // SUCCESS == 0 so need '!(fxn)' for correct if statements 
 #define ERROR 1
 #define SUCCESS 0
+#define FALSE 0
+#define TRUE 1
 
-// Checks whether 'c' is a digit 0-9.
+// Checks whether 'c' is a digit 0-9, return 1 if it is.
 int is_digit(char c) {
-    if (!((c>='0')&&(c<='9')))
-        return SUCCESS;
+    if (((c>='0')&&(c<='9')))
+        return TRUE;
     else
-        return ERROR;
+        return FALSE;
 }
 
 // Checks whether the input string is floating point number (must contain ".")
@@ -23,25 +24,26 @@ int is_floating_point_number(char* input){
     // c refers to the first character in the string "input".
     char c = *input;
     // Checks if first char is either a digit or '+'/'-'.
-    if (!(is_digit(c) || c == '+') || (c == '-'))
-        return ERROR;
+    if (!is_digit(c) && c != '+' && c != '-')
+        return FALSE;
     // Indicates the presence of the required '.' in the string.
-    bool dot = false;
-    for(int i = 1; input[i] !=0; i++) {
+    int dot = FALSE;
+    int i;
+    for (i = 1; input[i] !=0; i++) {
         c = input[i];
         if (c == '.') {
-            // If one '.' already found, return ERROR.
+            // If one '.' already found, return FALSE.
             if (dot)
-                return ERROR;
+                return FALSE;
             else
-                dot = true;
+                dot = TRUE;
             continue;
         }
-        // If not a digit, return ERROR.
+        // If not a digit, return FALSE.
         else if (!is_digit(c))
-            return ERROR;
+            return FALSE;
     }
-    return (dot ? SUCCESS : ERROR);
+    return (dot ? TRUE : FALSE);
 }
 
 // Checks if the string is an integer starting with '-'/'+' and containing all numerals.
@@ -49,15 +51,16 @@ int is_integer(char* input){
      // c refers to the first character in the string "input".
     char c = *input;
     // Checks if first char is either a digit or '+'/'-'.
-    if (!(is_digit(c) || (c == '+') || (c == '-')))
-        return ERROR;
-    for(int i = 1; input[i] !=0; i++) {
-        // If not a digit, return ERROR.
+    if (!is_digit(c) && c != '+' && c != '-')
+        return FALSE;
+    int i;
+    for (i = 1; input[i] !=0; i++) {
+        // If not a digit, return False.
         c = input[i];
-        if (is_digit(c) == SUCCESS)
-            return ERROR;
+        if (!is_digit(c))
+            return FALSE;
     }
-    return SUCCESS;
+    return TRUE;
 }
 
 // Checks if string ends in 'C' or 'F' then stores the string as a double in &temperature.
@@ -74,14 +77,13 @@ int validate_and_parse_input(char * input, double * temperature, char * scale) {
     strcpy(input_copy, input); 
     input_copy[length-1] = '\0';
     // Check if floating point
-    if (is_floating_point_number(input_copy) == SUCCESS)
+    if (is_floating_point_number(input_copy))
         *temperature = strtod(input_copy, NULL);
-    else if (is_integer(input_copy) == SUCCESS)
+    else if (is_integer(input_copy))
         *temperature = (double)atoi(input_copy);
     else 
         return ERROR;
     return SUCCESS;
-
 }
 
 // Converts celcius to fahrenheit.
@@ -97,6 +99,7 @@ double fahrenheit_to_celsius(double fahrenheit) {
 // Prints error message for bad input.
 void print_usage() {
     printf("\nERROR: PLEASE ENTER INPUT IN VALID FORMAT\n");
+    exit(EXIT_FAILURE);
 }
 
 // Prints correct output based on cases from validate_and_parse
@@ -122,10 +125,8 @@ int main(int argc, char *argv[]) {
         int result = validate_and_parse_input(input, &temperature, &scale);
         if(result == ERROR) {
             print_usage();
-            return 0;
         }
         else {
-            printf("%c",scale);
             if (scale == 'C')
                 print_output(temperature,celsius_to_farenheit(temperature), scale);
             else
